d56a34fc1ee97d2364e337c65026c6aa904b93f5
brandonurtecho <brandon.urtecho@unmsm.edu.pe>
2018-07-25 16:06:08 -0500

    primer commit

---
+++ src/components/Example.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+      <h1>Heroes</h1>  
+      <div class="sorting">
+        <label for="filtername">Find your hero:</label>
+        <input v-model="findName" id="filtername" type="text" />
+      </div>    
+      <ul>
+        <li v-for="name in filteredNames" :key="name">
+          {{ name }}
+        </li>
+      </ul>
+    </div>    
+</template>
+
+<script>
+export default {
+  name: "Example",
+  data() {
+    return {
+      findName: "",
+      names: [
+        "Evan You",
+        "John Lindquist",
+        "Jen Looper",
+        "Miriam Suzanne",
+        "Chris Coyier",
+        "Geoff Graham",
+        "Divya Sasidharan",
+        "Lea Verou",
+        "Rachel Andrew",
+        "Vitaly Friedman",
+        "Ryan Florence",
+        "Dan Abramov",
+        "Jen Simmons",
+        "Robin Rendle",
+        "Nicole Sullivan"
+      ]
+    };
+  },
+  computed: {
+    filteredNames() {
+      let filter = new RegExp(this.findName, "i");
+      console.log("hola");
+      return this.names.filter(el => el.match(filter));
+    }
+  }
+};
+</script>
+
+
+<style>
+</style>
